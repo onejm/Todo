fbbd4d65539d327788a6efa099066c249fc05410
seaotter <jeongmin0324@gmail.com>
2025-06-05 11:56:49 +0900

    [Init] 프로젝트 생성

--- user_frontend/src/pages/TodoCalendarPage.jsx
+++ user_frontend/src/pages/TodoCalendarPage.jsx
@@ -1,9 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import FullCalendar from '@fullcalendar/react';
 import dayGridPlugin from '@fullcalendar/daygrid';
-import interactionPlugin from '@fullcalendar/interaction'; // ✅ 클릭 이벤트 지원
+import interactionPlugin from '@fullcalendar/interaction';
 import { useNavigate } from 'react-router-dom';
-import axios from '../api/axios'; // 커스텀 axios 인스턴스 사용
+import axios from '../api/axios';
 import '../styles/calendar.css';
 
 function TodoCalendarPage() {
@@ -28,16 +28,27 @@ function TodoCalendarPage() {
         fetchTodos();
     }, []);
 
+    // 우선순위별 색상 지정 함수
+    const getColorByPriority = (priority) => {
+        switch (priority) {
+            case 'HIGH': return '#ddb6b2';     // 빨강
+            case 'MEDIUM': return '#cdbda8';   // 주황
+            case 'LOW': return '#96bfa6';      // 초록
+            default: return '#a9c8dc';         // 파랑
+        }
+    };
+
     const events = todos.map((todo) => ({
         title: todo.title,
         start: todo.startDate,
         end: todo.endDate,
         allDay: true,
+        color: getColorByPriority(todo.priority),
+        textColor: '#ffffff'
     }));
 
     return (
         <div style={{ maxWidth: 1000, margin: '30px auto' }}>
-
             <FullCalendar
                 plugins={[dayGridPlugin, interactionPlugin]}
                 initialView="dayGridMonth"
